59e88158aa2f7a3009628916d27e9f41541287a8
itan <itan@itans-Mac.local>
2018-03-08 01:46:50 -0800

    added changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,46 @@
 $( document ).ready(function(){
   console.log("jQuery Works");
-  $("#Test").css("color","red")
-  $("h1").css("font-size","200pxs")
-  $("body").css("background-color","green")
+  var box = $("#myBox");
 
-//when you run a test #text. run function
-  $("#Test").click(function(){
-    $("h1").css("color","white");
+  box.mouseover(function(){
+
+    // console.log("over the box");
+    // box.hide(4000);
   });
-// get current value of #header color
-  var currentColor = $("header").css("color");
-//if header color is black
-  if (currentColor == "rgb(0, 0, 0)") {
-    //make it gold
-    $("#header").css("color","gold");
-  }else {
-    //..otherwise make it black again
-    $("#header").css("color","rgb(0, 0, 0)");
-  }
+
+  $("#buttonColor").click( function () {
+    box.show();
+    console.log(giveMeARandomNumber(),giveMeARandomNumber());
+    // 3 variation:
+
+      colorString =
+
+
+      // var colorString = "rgb("+ giveMeARandomNumber() + ", "+ giveMeARandomNumber() + ", " + giveMeARandomNumber()+")";
+      // console.log(colorString);
+      // box.css("background-color", colorString);
+
+      // or
+      //       box.css("background-color", "rgb("+ giveMeARandomNumber() + ", "+ giveMeARandomNumber() + ", " + giveMeARandomNumber()+")");
+  // });
+  $("#hide").click( function () {
+    box.hide();
+
+
+  });
+var loopingColor = setinterval(function(){
+
+
+          box.css("background-color", "rgb("+ giveMeARandomNumber() + ", "+ giveMeARandomNumber() + ", " + giveMeARandomNumber()+")");
+  },1000);
+
 
 
 });
+
+function giveMeARandomNumber(){
+  var number = Math.random()*255;
+    number = Math.round(number);
+
+  return number;
+}
